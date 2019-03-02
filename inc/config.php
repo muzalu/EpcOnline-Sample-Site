15f3086aced1536393566a809c0c775766da0b34
muzalu <russellnwyatt@gmail.com>
2019-03-02 18:00:38 +0000

    Update config.php

--- inc/config.php
+++ inc/config.php
@@ -1,6 +1,6 @@
 <?php
 	$_dealer_name = "Enter your Account name here";
-	$_dealer_key = 'd22150c7-bbec-43bd-9d3a-9baf452b8a31';
+	$_dealer_key = 'xxxxxxxx-xxxx-xxxx-xxxx-xxxxxxxxxxxx';
     $_product_key_honda = '2163ef55-5db0-4300-ba58-ea4303df5f46';
 	$_product_key_yamaha = '324d31df-d5ab-4449-85a7-43e5a97c3e28';
     $_configured_product_types_honda = "MB,MA,GP";
